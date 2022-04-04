0af850412964caa3c00c10752060c1ffdfcf262a
Andrea Crotti <andrea.crotti.@gmail.com>
2022-04-04 11:42:00 +0100

    ignored files

--- lein/project.clj
+++ lein/project.clj
@@ -1,6 +1,6 @@
 (defproject lein "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Project description"
+  :url "project-url"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.11.0"]]
